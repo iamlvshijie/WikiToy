37b547e9623ebc47878b2d459a2e03833cc42781
iamlvshijie <iamlvshijie@163.com>
2018-03-04 16:09:33 +0800

    add componet and example/audio_play; add keys int code

---
+++ fw_design/example/audio_play/main/main.c
@@ -0,0 +1,190 @@
+/* GPIO Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "freertos/event_groups.h"
+#include "wm8978.h"
+#include "esp_vfs_fat.h"
+#include "driver/sdmmc_host.h"
+#include "driver/sdmmc_defs.h"
+#include "sdmmc_cmd.h"
+#include "esp_event_loop.h"
+#include "esp_log.h"
+#include <sys/socket.h>
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "eth.h"
+#include "event.h"
+#include "wifi.h"
+#include "hal_i2c.h"
+#include "hal_i2s.h"
+#include "wm8978.h"
+#include "webserver.h"
+#include "http.h"
+#include "cJSON.h"
+#include "mdns_task.h"
+#include "audio.h"
+#include <dirent.h>
+#include "esp_heap_caps.h"
+#include "euler.h"
+#include "websocket.h"
+#include "esp_heap_caps.h"
+#include "aplay.h"
+
+
+#define TAG "main:"
+
+
+#define LED_GPIO_OUTPUT_IO    5
+#define LED_GPIO_OUTPUT_PIN_SEL  ((1<<LED_GPIO_OUTPUT_IO))
+
+void led_init()
+{
+	/*init gpio for led*/
+    gpio_config_t io_conf;
+    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = LED_GPIO_OUTPUT_PIN_SEL;
+    io_conf.pull_down_en = 0;
+    io_conf.pull_up_en = 0;
+    gpio_config(&io_conf);
+    gpio_set_level(LED_GPIO_OUTPUT_IO, 1);
+}
+
+#define GPIO_INPUT_IO_0     34
+#define GPIO_INPUT_IO_1     35
+#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1))
+#define ESP_INTR_FLAG_DEFAULT 0
+
+static xQueueHandle gpio_evt_queue = NULL;
+
+static void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+    uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+static void gpio_task_example(void* arg)
+{
+    uint32_t io_num;
+    for(;;) {
+        if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            printf("GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
+        }
+    }
+}
+void key_init()
+{
+	gpio_config_t io_conf;
+	
+    //interrupt of rising edge
+    io_conf.intr_type = GPIO_PIN_INTR_POSEDGE;
+    //bit mask of the pins, use GPIO34/35 here
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    //set as input mode    
+    io_conf.mode = GPIO_MODE_INPUT;
+    //enable pull-up mode
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
+
+    //change gpio intrrupt type for one pin
+    gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_ANYEDGE);
+
+    //create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    //start gpio task
+    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+
+    //install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void*) GPIO_INPUT_IO_1);
+
+    //remove isr handler for gpio number.
+    gpio_isr_handler_remove(GPIO_INPUT_IO_0);
+    //hook isr handler for specific gpio pin again
+    gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
+	
+	
+}
+
+void codec_init()
+{
+	/*init codec */
+    hal_i2c_init(0,19,18);
+    hal_i2s_init(0,48000,16,2);
+    WM8978_Init();
+    WM8978_ADDA_Cfg(1,1); 
+    WM8978_Input_Cfg(1,0,0);     
+    WM8978_Output_Cfg(1,0); 
+    WM8978_MIC_Gain(25);
+    WM8978_AUX_Gain(0);
+    WM8978_LINEIN_Gain(0);
+    WM8978_SPKvol_Set(0);
+    WM8978_HPvol_Set(15,15);
+    WM8978_EQ_3D_Dir(0);
+    WM8978_EQ1_Set(0,24);
+    WM8978_EQ2_Set(0,24);
+    WM8978_EQ3_Set(0,24);
+    WM8978_EQ4_Set(0,24);
+    WM8978_EQ5_Set(0,24);	
+}
+
+void sdcard_init()
+{
+	/*init sd card*/
+	esp_err_t err;
+    sdmmc_host_t host = SDMMC_HOST_DEFAULT();
+    sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
+    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+        .format_if_mount_failed = true,
+        .max_files = 10
+    };
+    sdmmc_card_t* card;
+    err = esp_vfs_fat_sdmmc_mount("/sdcard", &host, &slot_config, &mount_config, &card);
+    if (err != ESP_OK) {
+        if (err == ESP_FAIL) {
+            printf("Failed to mount filesystem. If you want the card to be formatted, set format_if_mount_failed = true.");
+        } else {
+            printf("Failed to initialize the card (%d). Make sure SD card lines have pull-up resistors in place.", err);
+        }
+        return;
+    }
+    sdmmc_card_print_info(stdout, card);	
+}
+void print_system_info()
+{
+    /*print ram info*/
+    size_t free8start=heap_caps_get_free_size(MALLOC_CAP_8BIT);
+    size_t free32start=heap_caps_get_free_size(MALLOC_CAP_32BIT);
+    ESP_LOGI(TAG,"free mem8bit: %d mem32bit: %d\n",free8start,free32start);
+}
+
+void app_main()
+{
+    print_system_info();
+    led_init();
+	codec_init();
+	sdcard_init();
+    
+    uint8_t cnt=0;
+    while(1){
+        gpio_set_level(LED_GPIO_OUTPUT_IO, cnt%2);
+        aplay_mp3("/sdcard/music.mp3");
+        aplay_wav("/sdcard/music.wav");
+        cnt++;
+    }
+}
+
