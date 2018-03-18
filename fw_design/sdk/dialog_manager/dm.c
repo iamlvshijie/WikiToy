0f040ba24b64348669771f129c33f36b93afd3ca
iamlv <iamlv@JACOBS-NUC>
2018-03-18 16:11:12 +0000

    add sheet entries

---
+++ fw_design/sdk/dialog_manager/dm.c
@@ -0,0 +1,26 @@
+
+uint32_t dm_get_mode()
+{
+
+
+
+}
+
+/*
+ state:bt_mode;local_audio_playing;online_audio_playing;online_chating;idle;
+ trigger:key_event;bt_event;online_push_event;
+*/
+void dm_state_machine_task(uint32_t)
+{
+
+
+}
+
+void dm_init()
+{
+    //check resources
+
+
+    
+
+}
